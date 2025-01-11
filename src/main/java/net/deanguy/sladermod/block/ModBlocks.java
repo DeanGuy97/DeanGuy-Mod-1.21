722f57bebb7b93d6101cff15a75c17f3d23940e9
DeanBoland <deanboland343@gmail.com>
2025-01-11 03:17:52 -0800

    Testing Commits

--- src/main/java/net/deanguy/sladermod/block/ModBlocks.java
+++ src/main/java/net/deanguy/sladermod/block/ModBlocks.java
@@ -16,8 +16,6 @@ import net.minecraftforge.registries.RegistryObject;
 
 import java.util.function.Supplier;
 
-//testing commits
-
 public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, SladerMod.MOD_ID);
